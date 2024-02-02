c364bb26d45479e60ef8dfad259a24f24cbd45aa
Thomaz Juliann Boncompagni <thomaz.jb@hotmail.com>
2024-02-02 14:57:37 -0300

    imprroving meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 const Navbar = dynamic(() => import('../components/Navbar'), { ssr: false })
@@ -12,6 +13,10 @@ export default function RootLayout({
 }) {
   return (
      <html lang="en">
+       <Head>
+        <meta name="description" content="Chainspoke Chart Page Test" />
+        <meta name="author" content="Thomaz Juliann Boncompagni" />
+      </Head>
       <body className={inter.className}><Navbar></Navbar>{children}</body>
     </html>
 
